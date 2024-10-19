c4713253e788490a1c292f0c6ac3b16e2136721f
Chamaththa123 <shamodchamaththa@gmail.com>
2024-10-19 10:45:49 +0530

    init

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP Project</title>
+    <!-- <link rel="stylesheet" href="public/assets/css/styles.css"> Link to your CSS file -->
+</head>
+
+<body>
+
+    <!-- Header Section -->
+    <?php include 'src/includes/header.php'; ?>
+    <!-- Main Content Section -->
+    <main>
+        <h1>Welcome to My PHP Project</h1>
+        <p>This is a simple demonstration of connecting to a MySQL database.</p>
+
+        <!-- Data Display Section -->
+        <h2>Data from Database:</h2>
+
+    </main>
+
+    <!-- Footer Section -->
+
+    <!-- <script src="public/assets/js/scripts.js"></script> Link to your JS file -->
+</body>
+
+</html>
